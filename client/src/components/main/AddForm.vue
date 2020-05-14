f82a45a144d46d2eeec37d50040fdd9ab1f27a78
ELBEQQAL94 <ELBEQQAL.youssef@gmail.com>
2020-05-14 16:08:58 +0000

    make simple changes

--- client/src/components/main/AddForm.vue
+++ client/src/components/main/AddForm.vue
@@ -4,39 +4,54 @@
       <div class="form-group">
         <label for="title">Title</label>
        <input
-          v-model="title"
+          v-model="book.title"
           type="text"
           class="form-control"
           id="title"
           aria-describedby="title"
         />
-        <small id="title" class="form-text text-muted">Show Errors</small>
+      <small
+        v-if="errors.isError"
+        class="form-text text-muted error"
+      >{{errors.title}}</small>
       </div>
       <div class="form-group">
         <label for="author">Author</label>
         <input
-          v-model="author"
+          v-model="book.author"
           type="text"
           class="form-control"
           id="author"
         />
+        <small
+        v-if="errors.isError"
+        class="form-text text-muted error"
+      >{{errors.author}}</small>
       </div>
       <div class="form-group">
         <label for="image-url">Image Url</label>
         <input
-          v-model="imageUrl"
+          v-model="book.imageUrl"
           type="text"
           class="form-control"
           id="image-url"
         />
+        <small
+        v-if="errors.isError"
+        class="form-text text-muted error"
+      >{{errors.imageUrl}}</small>
       </div>
       <div class="form-group">
         <label for="description">Description</label>
         <textarea
-          v-model="description"
+          v-model="book.description"
           class="form-control"
           id="description"
           rows="3"></textarea>
+          <small
+        v-if="errors.isError"
+        class="form-text text-muted error"
+      >{{errors.description}}</small>
       </div>
     </div>
     <div class="modal-footer">
@@ -52,8 +67,10 @@ export default {
   name: 'AddForm',
   data() {
     return {
-      errors: [],
-      user: {
+      errors: {
+        isError: false,
+      },
+      book: {
         title: '',
         author: '',
         description: '',
@@ -63,26 +80,51 @@ export default {
   },
   methods: {
     addBook() {
-      // if (this.title && this.author && this.description && this.imageUrl) {
-      //   return true;
-      // }
+      const {
+        title,
+        author,
+        description,
+        imageUrl,
+      } = this.book;
 
-      // this.errors = [];
+      if (title.trim().length === 0) {
+        this.errors.title = 'Title required.';
+        this.errors.isError = true;
+      } else {
+        this.errors.title = '';
+        this.errors.isError = false;
+      }
 
-      // if (!this.title) {
-      //   this.errors.push('Title required.');
-      // }
-      // if (!this.author) {
-      //   this.errors.push('Author required.');
-      // }
-      // if (!this.description) {
-      //   this.errors.push('Description required.');
-      // }
-      // if (!this.imageUrl) {
-      //   this.errors.push('imageUrl required.');
-      // }
-      // return false;
-      console.log(this.user);
+      if (author.trim().length === 0) {
+        this.errors.author = 'Author required.';
+        this.errors.isError = true;
+      } else {
+        this.errors.author = '';
+        this.errors.isError = false;
+      }
+
+      if (description.trim().length === 0) {
+        this.errors.description = 'Description required.';
+        this.errors.isError = true;
+      } else {
+        this.errors.description = '';
+        this.errors.isError = false;
+      }
+
+      if (imageUrl.trim().length === 0) {
+        this.errors.imageUrl = 'imageUrl required.';
+        this.errors.isError = true;
+      } else {
+        this.errors.imageUrl = '';
+        this.errors.isError = false;
+      }
+
+      if (this.errors.isError) {
+        console.log('Something Wrong...');
+      } else {
+        this.$store.dispatch('addBook', this.book);
+        console.log('Book Added...');
+      }
     },
   },
 };
